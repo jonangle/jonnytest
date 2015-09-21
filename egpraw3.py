c066776c6c345afa53571dcd2606f86005ea4765
jonangle <jonangle1991@gmail.com>
2015-09-21 23:45:14 +0800

    hihi
    try

---
+++ egpraw3.py
@@ -0,0 +1,5 @@
+# Writing a bot
+# Bot runs continuously whereas scripts are one-off jobs
+# Bot automate some task that could be performed by a user (posting, commenting)
+# Make sure bot does not perform same task twice
+
